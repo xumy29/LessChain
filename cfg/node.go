3f999670f9ae9e43cb528464e198f743c0f5e986
xumy29 <xumy29@mail2.sysu.edu.cn>
2023-10-13 19:04:12 +0800

    修改节点地址配置

--- cfg/node.go
+++ cfg/node.go
@@ -19,20 +19,36 @@ func init() {
 	ClientTable = make(map[uint32]string)
 	ClientTable[0] = "192.168.3.2:" + fmt.Sprint(19000)
 
+	// 从50002开始配置分片节点
 	// 配置每个分片20个节点的地址
 	NodeTable = make(map[uint32]map[uint32]string)
 
-	startIP := 3
-	startPort := 20000
-	shardPerMachine := 2
+	hostTable := make(map[uint32]string)
+	startPortTable := make(map[uint32]uint32)
+	hostTable[0] = "192.168.3.3"
+	startPortTable[0] = 20000
+	hostTable[1] = "192.168.3.4"
+	startPortTable[1] = 20000
+	hostTable[2] = "192.168.3.5"
+	startPortTable[2] = 20000
+	hostTable[3] = "192.168.3.6"
+	startPortTable[3] = 20000
+	hostTable[4] = "192.168.3.7"
+	startPortTable[4] = 20000
+	hostTable[5] = "192.168.3.8"
+	startPortTable[5] = 20000
+	hostTable[6] = "192.168.3.9"
+	startPortTable[6] = 20000
+	hostTable[7] = "192.168.3.10"
+	startPortTable[7] = 20000
+	hostTable[8] = "192.168.3.11"
+	startPortTable[8] = 20000
+
 	var i, j uint32
 	for i = 0; i < 9; i++ {
 		NodeTable[i] = make(map[uint32]string)
-		// 从50002开始配置分片节点
-		host := fmt.Sprintf("192.168.3.%d", startIP+int(i)/shardPerMachine)
 		for j = 0; j < 20; j++ {
-			NodeTable[i][j] = host + ":" + fmt.Sprint(startPort)
-			startPort += 1
+			NodeTable[i][j] = hostTable[i] + ":" + fmt.Sprint(startPortTable[i]+j)
 		}
 	}
 
