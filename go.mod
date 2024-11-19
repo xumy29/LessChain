edc20ac5e6f6433281f4e73d68558b9a54ec2b04
xumy <xumy29@mail2.sysu.edu.cn>
2024-11-19 20:08:46 +0800

    确保有安装合适的go版本并设置路径

--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d
 	github.com/schollz/progressbar/v3 v3.10.1
 	github.com/spf13/pflag v1.0.3
-	github.com/stretchr/testify v1.8.4
 	github.com/vechain/go-ecvrf v0.0.0-20220525125849-96fa0442e765
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
 )
@@ -30,16 +29,15 @@ require (
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/tsdb v0.7.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rjeczalik/notify v0.9.1 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
 	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
 	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- start_lessChain_for_linux.sh
+++ start_lessChain_for_linux.sh
@@ -11,6 +11,34 @@ SHARD_ALL_NODE_NUM=$(jq .ComAllNodeNum $CONFIG_FILE)
 MACHINE_NUM=$(jq .MachineNum $CONFIG_FILE)
 SHARD_START_INDEX=$(jq .ShardStartIndex $CONFIG_FILE)
 ROLE=$(jq -r .Role $CONFIG_FILE)
+# 如果系统上的go版本大于等于1.17，则直接使用，否则安装go1.21
+## 检查系统是否安装 Go 以及 Go 版本是否大于等于 1.17
+if ! go version &> /dev/null || [[ "$(go version | awk '{print $3}')" < "go1.17" ]]; then
+    # 设置 Go 安装目录
+    GO_DIR=$HOME/xmyWorkspace/software
+
+    echo "系统没有安装 Go 或 Go 版本小于 1.17，开始安装 Go 1.21.1..."
+
+    # 下载 Go 1.21.1
+    wget https://mirrors.aliyun.com/golang/go1.21.1.linux-amd64.tar.gz -P /tmp
+
+    # 解压 Go 到指定目录
+    mkdir -p $GO_DIR
+    sudo tar -C $GO_DIR -xzf /tmp/go1.21.1.linux-amd64.tar.gz
+
+    # 清理下载的文件
+    rm /tmp/go1.21.1.linux-amd64.tar.gz
+
+    # 设置 GO 环境变量
+    echo "export PATH=$GO_DIR/go/bin:$PATH" >> ~/.bashrc
+
+    # 重新加载 shell 配置
+    source ~/.bashrc
+fi
+
+# 确认 Go 版本
+echo "Go 版本：$(go version)"
+GO=go
 
 if [ "$ROLE" != "node" ]
 then
@@ -28,14 +56,14 @@ then
 
     # 启动其他终端并运行相应的命令
     echo "Starting booter..."
-    screen -d -m bash -c "go build -o ./lessChain && ./lessChain -r booter -S $SHARD_NUM; tail -f /dev/null"
+    screen -d -m bash -c "$GO build -o ./lessChain && ./lessChain -r booter -S $SHARD_NUM; tail -f /dev/null"
     sleep 5
 
     echo "Starting client..."
     screen -d -m bash -c "./lessChain -r client -S $SHARD_NUM; tail -f /dev/null"
     sleep 1
 else
-    go build -o ./lessChain
+    $GO build -o ./lessChain
     sleep 5
     # 启动分片节点
     for ((j=0;j<$((SHARD_NUM / MACHINE_NUM));j++));
